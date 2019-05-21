e0a65a007cc851d960767517a9ebbeb45fbceae7
michael-basweti <baswetima@gmail.com>
2019-05-21 11:04:19 +0300

    debugging

--- lib/products.dart
+++ lib/products.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   final List<String> products;
 
-  Products(this.products);
+  Products([this.products = const []]){
+    print('[profucts widget] Constructor');
+  }
   @override
   Widget build(BuildContext context) {
+    print('[profucts widget] BUILD');
     return Column(
                   children: products
                       .map((element) => Card(
